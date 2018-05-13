d9e5823f243e13724c90b6b4a5eaa76ea8e527ac
Arun Patel <arunsmtp@gmail.com>
2018-05-13 16:59:50 +0100

    changed docker run as current user

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 # Runs flowblade in a docker container
 # Flowblade's config file is in the CWD
 # Tested briefly with nvidia and intel gpus
-# 
+#
 # arunsmtp@gmail.com
 #
 
-DockerImage="flowblade-1.14:latest"
+DockerImage="flowblade-1.16:latest"
 CPUS="3"
 MEM="4g"
 
@@ -15,15 +15,19 @@ MEM="4g"
   docker run --rm -it \
        --cpus=${CPUS} \
        --memory=${MEM} \
+       --user ${UID}:${UID} \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -e DISPLAY=unix$DISPLAY \
        --device /dev/dri \
-       --device /dev/snd \
-       -v /dev/shm:/dev/shm \
        -v ${PWD}:/workdir \
-       -v ${PWD}/.flowblade:/root/.flowblade \
-       --name flowblade-1.14 \
+       --name Flowblade \
+       -v /etc/passwd:/etc/passwd:ro \
+       -v /etc/group:/etc/group:ro \
+       -v ${PWD}/fbhome/:/home/${USER}/:rw \
+       --device /dev/snd \
+       --group-add audio \
+       -v /dev/shm:/dev/shm \
     ${DockerImage}
 
- 
+##        -v /dev/shm:/dev/shm \
 
